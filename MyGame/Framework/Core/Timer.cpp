55a825fe52aa3267b8376c8f7f7cc593049b783a
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-01-26 21:39:53 +0700

    create Timer class

---
+++ MyGame/Framework/Core/Timer.cpp
@@ -0,0 +1,79 @@
+#include "Timer.h"
+
+Timer::Timer()
+{
+	startTime = 0;
+	currentTime = 0;
+	previousTime = 0;
+	deltaTime = 0;
+	isStopped = true;
+}
+
+double Timer::getTotalTime() const
+{
+	return (currentTime - startTime);
+}
+
+double Timer::getDeltaTime() const
+{
+	return deltaTime;
+}
+
+void Timer::Start()
+{
+	if (isStopped)
+	{
+		startTime = GetTickCount64();
+		previousTime = startTime;
+		isStopped = false;
+		//log
+		return;
+	}
+	else
+	{
+		// log
+		return;
+	}
+}
+
+void Timer::Reset()
+{
+	startTime = GetTickCount64();
+	previousTime = startTime;
+	isStopped = false;
+	// log
+}
+
+void Timer::Tick()
+{
+	if (isStopped)
+	{
+		deltaTime = 0;
+		// log
+		return;
+	}
+	else
+	{
+		deltaTime = (currentTime - previousTime);
+		previousTime = currentTime;
+		if (deltaTime < 0.0)
+			deltaTime = 0.0;
+		// log
+		return;
+	}
+}
+
+void Timer::Stop()
+{
+	if (!isStopped)
+	{
+		isStopped = true;
+		// log
+		return;
+	}
+	else
+	{
+		// log
+		return;
+	}
+}
---
+++ MyGame/Framework/Core/Timer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <d3d9.h>
+#include <d3dx9.h>
+
+class Timer
+{
+public:
+	Timer();
+	double getTotalTime() const;
+	double getDeltaTime() const;
+	void Start();
+	void Reset();
+	void Tick();
+	void Stop();
+
+private:
+	long long int startTime;
+	long long int currentTime;
+	long long int previousTime;
+	double deltaTime;
+	bool isStopped;
+};
