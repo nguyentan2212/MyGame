1136ec60a390a24982d60f7051ab4713644f541d
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-03-08 12:24:44 +0700

    use TestGame class

--- MyGame/MyGame.cpp
+++ MyGame/MyGame.cpp
@@ -12,11 +12,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    Game game(hInstance, nCmdShow);
-    while (game.IsRunning())
+    Game* game;
+    game = new TestGame(hInstance, nCmdShow);
+    while (game->IsRunning())
     {
-        game.Update();
-        game.LateUpdate();
-        game.Draw();
+        game->Update();
+        game->LateUpdate();
+        game->Draw();
     }
 }
