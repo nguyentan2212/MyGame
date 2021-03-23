f6d6c12701b1320712ece0c0a5fa5bbe5f0485dc
nguyentan2212 <57379741+nguyentan2212@users.noreply.github.com>
2021-03-23 15:58:03 +0700

    add GetKeyCode and getKeyState

--- MyGame/Framework/Core/KeyInfo.h
+++ MyGame/Framework/Core/KeyInfo.h
@@ -7,6 +7,8 @@ public:
 	KeyInfo();
 	KeyInfo(int keyCode, KeyState keyState);
 	~KeyInfo(){}
+	int GetKeyCode() const { return keyCode; }
+	KeyState GetKeyState() const { return keyState; }
 private:
 	int keyCode;
 	KeyState keyState;
